6b8f892a5308c0047e27a64f7411a3d5d63845a8
mvg-v70 <gusev-mv@yandex.ru>
2015-07-25 19:32:53 +0300

    1.5

---
+++ src/com/mvgv70/mtc_app_copy/InterfaceButton.java
@@ -0,0 +1,16 @@
+package com.mvgv70.mtc_app_copy;
+
+import android.content.Context;
+import android.widget.ImageButton;
+
+public class InterfaceButton extends ImageButton {
+	
+  public String appPath;
+  public String appName;
+	
+  public InterfaceButton(Context context) 
+  {
+    super(context);
+  }
+
+}
